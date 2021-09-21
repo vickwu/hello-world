34bfa6464239a49cbea98fa30ec04714f69aec50
vickwu <vickwu1988@gmail.com>
2021-09-21 16:18:07 +0000

    add ML packages

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 # %% Load package
 from flask import Flask, request,jsonify
 from flask_restful import Resource, Api
+from joblib import load
+import xgboost as xgb
+import pandas as pd
+MODEL_DIR = 'model/'
+MODEL_FILE = 'bst.joblib'
+DICT_FILE = 'reason_dic.joblib'
+SHAP_FILE = 'explainer.joblib'
+
+# %% Load modeling and ARR info
+MODEL_PATH=MODEL_DIR+MODEL_FILE
+DICT_PATH=MODEL_DIR+DICT_FILE
+SHAP_PATH=MODEL_DIR+SHAP_FILE
+# Load model structure file
+mdl= load(MODEL_PATH)
+# load reason_code
+reason_dict= load(DICT_PATH)
+# Load SHAP value
+explainer= load(SHAP_PATH)
 # %% create service
 app = Flask(__name__)
 api = Api(app)
