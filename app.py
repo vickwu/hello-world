23ce125df7d24b67ecc3a01616c0cfe85b82234a
VickWu <60106286+vickwu@users.noreply.github.com>
2021-09-21 21:13:28 -0400

    add more

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import shap
 MODEL_DIR = 'model/'
 MODEL_FILE = 'bst.joblib'
 DICT_FILE = 'reason_dic.joblib'
-SHAP_FILE = 'explainer.joblib'
+# SHAP_FILE = 'explainer.joblib'
 # %% Load modeling and ARR info
 MODEL_PATH=MODEL_DIR+MODEL_FILE
 DICT_PATH=MODEL_DIR+DICT_FILE
@@ -18,7 +18,8 @@ mdl= load(MODEL_PATH)
 # load reason_code
 reason_dict= load(DICT_PATH)
 # Load SHAP value
-explainer= load(SHAP_PATH)
+explainer = shap.TreeExplainer(mdl)
+
 # %% create service
 app = Flask(__name__)
 api = Api(app)
@@ -32,12 +33,11 @@ class prediction(Resource):
         preds = mdl.predict(dX)
         Score=int(preds[0]*10000)/100
         SHAP_VALUE=X.T
-        # shap_value = explainer(X)
-        # SHAP_VALUE['SHAP_contri']=list(shap_value[0].values)
+        shap_value = explainer(X)
+        SHAP_VALUE['SHAP_contri']=list(shap_value[0].values)
         SHAP_VALUE['AAR']=SHAP_VALUE.index.map(reason_dict)
-        # SHAP_VALUE=SHAP_VALUE.sort_values(by='SHAP_contri', ascending=False)
+        SHAP_VALUE=SHAP_VALUE.sort_values(by='SHAP_contri', ascending=False)
         unique_code=list(SHAP_VALUE.AAR.drop_duplicates())
-        # unique_code=['placeholder','placeholder','placeholder','placeholder']
         return {'prediction(PD%)': Score,
         'reason_Code1':unique_code[0],
         'reason_Code2':unique_code[1],
